34f3de0c14868aece51d286e4cde583437b29de4
laisamartini <149402979+laisamartini@users.noreply.github.com>
2025-12-11 17:04:29 -0400

    Update delnoticias.js

--- src/utils/delnoticias.js
+++ src/utils/delnoticias.js
@@ -1,7 +1,7 @@
 export async function delNoticiaRequest(idnoticia) {
     const body = { _id: idnoticia };
     try {
-        const response = await fetch(`https://api6anoticias.vercel.app/delnoticias`, {
+        const response = await fetch(`https://fwapi-ashen.vercel.app/delnoticias`, {
             method: "DELETE",
             headers: {
                 "Content-Type": "application/json"
